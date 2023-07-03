a77138b99a19caac3063b2b502ad62f9d73656c9
Adhmir Gomes <55671798+Adhmir@users.noreply.github.com>
2023-07-03 14:39:11 -0300

    Update 2_Cite_Us.py

--- pages/2_Cite_Us.py
+++ pages/2_Cite_Us.py
@@ -9,7 +9,7 @@ st.set_page_config(
 
 st.title('How cite us')
 cont1 =st.container()
-cont1.write('Gomes, A. R. V. & Hein, N. (2023). Multiple Criteria Perfomance Measures - EDAS [Web streamlit app]. https://performancemesasures-edas.streamlit.app/')
+cont1.write('Gomes, A. R. V. & Hein, N. (2023). Multiple Criteria Perfomance Measures - EDAS [Web streamlit app]. https://performancemeasures-edas.streamlit.app/')
 
             
 st.subheader('About the Authors')
